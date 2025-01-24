2a8d9262c0d57aad730c8f25cf6cffa64882450f
Austin Nelson <austinn243@gmail.com>
2025-01-23 19:53:48 -0700

    [feat-6]: Complete part 2 of day 6 challenge
    Completed the second part of the day 6 challenge by parameterizing
    the size of the marker to look for.
    
    Since the two types of markers are represented in the same way,
    simply making the size changeable is all that is needed.

--- day6/main.py
+++ day6/main.py
@@ -11,7 +11,8 @@ INPUT_FILE = "input.txt"
 TEST_FILE = "test.txt"
 
 
-START_OF_MARKER_SIZE = 4
+START_OF_MESSAGE_MARKER_SIZE = 14
+START_OF_PACKET_MARKER_SIZE = 4
 
 
 def read_data(file_path: str) -> str:
@@ -21,28 +22,27 @@ def read_data(file_path: str) -> str:
         return file.read().strip()
 
 
-def find_first_start_of_packet_marker_position(data_stream: str) -> int:
-    """Find the position of the first start-of-packet marker in a data stream.
+def find_first_marker_position(data_stream: str, marker_size: int) -> int:
+    """Find the position of the first marker of the given size in a data stream.
 
-    The start-of-packet marker is indicated by string of four consecutive,
-    distinct characters.
+    Markers of size N are indicated by a sequence of N distinct, consecutive characters.
     """
 
     # NOTE: We explicitly handle the first three characters of the data stream
     # so that we can handle the fourth character in the loop rather than having
     # to handle it separately.
 
-    recent_character_counts = Counter(data_stream[: START_OF_MARKER_SIZE - 1])
-    index = START_OF_MARKER_SIZE - 1
+    recent_character_counts = Counter(data_stream[: marker_size - 1])
+    index = marker_size - 1
 
     while index < len(data_stream):
         current_character = data_stream[index]
         recent_character_counts[current_character] += 1
 
-        if len(recent_character_counts) == START_OF_MARKER_SIZE:
+        if len(recent_character_counts) == marker_size:
             return index + 1
 
-        oldest_character = data_stream[index - START_OF_MARKER_SIZE + 1]
+        oldest_character = data_stream[index - marker_size + 1]
         recent_character_counts[oldest_character] -= 1
         if recent_character_counts[oldest_character] == 0:
             del recent_character_counts[oldest_character]
@@ -55,16 +55,24 @@ def find_first_start_of_packet_marker_position(data_stream: str) -> int:
 def main() -> None:
     """Read a data stream from a file and process it."""
 
-    input_file = INPUT_FILE
+    input_file = TEST_FILE
     file_path = path.join(path.dirname(__file__), input_file)
 
     data_stream = read_data(file_path)
 
-    start_of_packet_marker_position = find_first_start_of_packet_marker_position(
+    start_of_packet_marker_position = find_first_marker_position(
         data_stream,
+        START_OF_PACKET_MARKER_SIZE,
     )
-    print("The first SOP marker is at position: ", end="")
+    print("The first start of packet marker is at position: ", end="")
     print(start_of_packet_marker_position)
+
+    start_of_message_marker_position = find_first_marker_position(
+        data_stream,
+        START_OF_MESSAGE_MARKER_SIZE,
+    )
+    print("The first start of message marker is at position: ", end="")
+    print(start_of_message_marker_position)
 
 
 if __name__ == "__main__":
